2443a459eb5bff8f127e9be83dd086527bd2efbc
yassinelogtari <60779968+yassinelogtari@users.noreply.github.com>
2022-04-06 23:33:49 +0100

    Add files via upload

---
+++ facebook-login/config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  'facebookAuth': {
+    'clientID': '326703742889397', // your App ID
+    'clientSecret': '1e2312f3ca3e1f04483c7ca5fa353637', // your App Secret
+    'callbackURL': 'http://localhost:3000/auth/facebook/callback'
+  }
+}
+
